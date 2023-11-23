d7a79fdc4055ef0da2e6064695243af71fe40182
pacoyang <yangbaigao@gmail.com>
2023-11-23 10:16:13 +0800

    style: formatting

--- src/viem/coder.ts
+++ src/viem/coder.ts
@@ -1,17 +1,17 @@
 import {decodeAbiParameters, encodeAbiParameters, parseAbiParameters} from "viem";
 
-export type HexString = `0x${string}`
+export type HexString = `0x${string}`;
 export const encodeReplyAbiParams = 'uint respType, uint id, uint256 data';
 export const decodeRequestAbiParams = 'uint id, string reqData';
 
 export function encodeReply(abiParams: string, reply: [bigint, bigint, bigint]): HexString {
-    return encodeAbiParameters(parseAbiParameters(abiParams),
-        reply
-    );
+  return encodeAbiParameters(parseAbiParameters(abiParams),
+    reply
+  );
 }
 
 export function decodeRequest(abiParams: string, request: HexString): any {
-    return decodeAbiParameters(parseAbiParameters(abiParams),
-        request
-    );
+  return decodeAbiParameters(parseAbiParameters(abiParams),
+    request
+  );
 }
